e7acb818f35e1445b0f01a378e12f0bff6118d02
GoSh069 <gogogospodinov69@gmail.com>
2020-11-27 23:29:04 +0200

    put getters and setters in class Quote

--- app/src/main/java/com/example/dailysmarts/data/database/Quote.java
+++ app/src/main/java/com/example/dailysmarts/data/database/Quote.java
@@ -23,4 +23,24 @@ public class Quote {
         this.quote = quote;
         this.author = author;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
 }
